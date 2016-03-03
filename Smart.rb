6166bf89b24e63fa0aca5ab91a9c8e69d25315be
MitchGianoni <mitchgianoni@yahoo.com>
2016-03-03 00:28:36 -0500

    corrected smart.rb so that the computer doesnt guess the same number twice

--- Smart.rb
+++ Smart.rb
@@ -7,9 +7,9 @@ class SmartPlayer
 	 	if guess == 0
 	 		@choice = rand(1..max)
 	 	elsif guess > answer
-	 		@choice = rand(min+1..guess)
+	 		@choice = rand(min..(guess-1))
 	 	elsif guess < answer
-	 		@choice = rand(guess..max-1)
+	 		@choice = rand((guess+1)..max)
 	 	end
 	end
 end
